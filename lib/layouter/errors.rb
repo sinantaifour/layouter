bb010aba12be863bc46a1d0b426c72fc70e048c8
Sinan Taifour <sinan@taifour.com>
2021-01-04 00:21:30 +0200

    Document the different errors.

--- lib/layouter/errors.rb
+++ lib/layouter/errors.rb
@@ -1,11 +1,15 @@
 module Layouter
 
+  # Root error, never raised.
   class GenericError < StandardError; end
 
+  # Raised if the state of an object is inconsistent with the called method.
   class StateError < GenericError; end
 
+  # Should never be raised, unless our code is buggy.
   class AssertionError < GenericError; end
 
+  # Only raised while laying out, if it is not possible to layout.
   class LayoutError < GenericError
 
     attr_reader :dimension, :reason
